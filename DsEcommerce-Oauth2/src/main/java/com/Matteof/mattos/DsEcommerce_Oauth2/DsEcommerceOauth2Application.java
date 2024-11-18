856353a439449250cf666a2dc6c10941264e867d
Matheus <matheus.oli2016@outlook.com>
2024-11-18 10:57:56 -0300

    0

---
+++ DsEcommerce-Oauth2/src/main/java/com/Matteof/mattos/DsEcommerce_Oauth2/DsEcommerceOauth2Application.java
@@ -0,0 +1,13 @@
+package com.Matteof.mattos.DsEcommerce_Oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsEcommerceOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsEcommerceOauth2Application.class, args);
+	}
+
+}
---
+++ DsEcommerce-Oauth2/src/test/java/com/Matteof/mattos/DsEcommerce_Oauth2/DsEcommerceOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Matteof.mattos.DsEcommerce_Oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsEcommerceOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
